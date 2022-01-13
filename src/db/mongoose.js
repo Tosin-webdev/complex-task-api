7da6dd6997cb176fc675d2ce1a8fad799d41d33d
Tosin <oladejit3@gmail.com>
2022-01-13 08:53:55 +0100

    removed irrelevant comment

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-// MONGO_URI =
-//   "mongodb+srv://user2:simple03@node-tutorial.rsb65.mongodb.net/newUser?retryWrites=true&w=majority";
 
 const connectDB = async () => {
   try {
@@ -9,7 +7,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`mongoDB connected: ${con.connection.host}`);
+    // console.log(`mongoDB connected: ${con.connection.host}`);
   } catch (err) {
     console.log(err);
     process.exit(1);
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,13 @@
 const request = require("supertest");
+const app = require("../index");
+
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      name: "Andrewin",
+      email: "andrew@placde.com",
+      password: "MyPass777tru!",
+    })
+    .expect(201);
+});
