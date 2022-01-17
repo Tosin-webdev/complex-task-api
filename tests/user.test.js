e3f11a68966e4c09d55d71e077158c23af1c23eb
Tosin <oladejit3@gmail.com>
2022-01-17 01:40:35 +0100

    updated file

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,13 +1,69 @@
 const request = require("supertest");
+const mongoose = require("mongoose");
 const app = require("../index");
+const User = require("../src/models/user");
+const jwt = require("jsonwebtoken");
 
-test("Should signup a new user", async () => {
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+  name: "feal",
+  email: "feal@gmail.com",
+  password: "comeonboys",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
+};
+
+// beforeEach(async () => {
+//   // console.log("beforeEach");
+//   await User.deleteMany();
+//   await new User(userOne).save();
+// });
+
+// afterAll(async () => {
+//   await mongoose.connection.close();
+// });
+// afterEach(() => {
+//   console.log("afterEach");
+// });
+
+test("should signup a new user", async () => {
   await request(app)
     .post("/users")
     .send({
-      name: "Andrewin",
-      email: "andrew@placde.com",
-      password: "MyPass777tru!",
+      name: "Andrew",
+      email: "pet@gmail.com",
+      password: "indeed01",
     })
     .expect(201);
+});
+
+test("Should login existing user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+});
+
+test("should not login non existence user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: "skjdnkjfme",
+    })
+    .expect(400);
+});
+
+test("should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
 });
