72b07454cd5d8d3524d41df24fb735cb5c885ed1
Oladeji Tosin <64624808+Tosin-webdev@users.noreply.github.com>
2022-02-16 07:59:43 +0100

    Update task.js

--- src/routes/task.js
+++ src/routes/task.js
@@ -56,7 +56,6 @@ router.get("/tasks/:id", auth, async (req, res) => {
     if (!task) {
       return res.status(404).send();
     }
-
     res.send(task);
   } catch (e) {
     res.status(500).send();
